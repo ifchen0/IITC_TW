39286166ffe476f87325940a22576c698c9f5ce1
iF <ifchen0@gmail.com>
2015-10-11 20:54:25 +0800

    Update IITC logo.

--- build/mobile/user-location.meta.js
+++ build/mobile/user-location.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-user-location@cradle
 // @name           IITC plugin: User Location
 // @category       Tweaks
-// @version        0.2.0.20151011.32532
+// @version        0.2.0.20151011.125259
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/mobile/user-location.meta.js
 // @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/mobile/user-location.user.js
-// @description    [mobile-2015-10-11-032532] Show user location marker on map
+// @description    [mobile-2015-10-11-125259] Show user location marker on map
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
