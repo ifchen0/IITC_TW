3a2c3cfda56d5cd5d52fa24eb7585593642e1287
iF <ifchen0@gmail.com>
2015-10-15 09:19:00 +0800

    Merged revision(s) 5870 from s-intel-total-conversion/trunk:

--- build/local/total-conversion-build.meta.js
+++ build/local/total-conversion-build.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @id             ingress-intel-total-conversion@jonatkins
 // @name           IITC: Ingress intel map total conversion
-// @version        0.25.2.20151012.84049
+// @version        0.25.2.20151015.11817
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/local/total-conversion-build.meta.js
 // @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/local/total-conversion-build.user.js
-// @description    [local-2015-10-12-084049] Total conversion for the ingress intel map.
+// @description    [local-2015-10-15-011817] Total conversion for the ingress intel map.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
