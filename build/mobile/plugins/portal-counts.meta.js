9846b7b06bfa7a2699d4bb7df6117e47eb8f5e92
iF <ifchen0@gmail.com>
2015-11-09 09:23:58 +0800

    Add portal counts plug-in for IITCm.

---
+++ build/mobile/plugins/portal-counts.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-portals-count@yenky
+// @name           IITC plugin: Show total counts of portals
+// @category       Info
+// @version        0.1.2.20151109.12232
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/mobile/plugins/portal-counts.meta.js
+// @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/mobile/plugins/portal-counts.user.js
+// @description    [mobile-2015-11-09-012232] 依照該區域的Portal等級和陣營顯示圖表.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
