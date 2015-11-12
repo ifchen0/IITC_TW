4ccf5e5be655e0972469da63ad21c7ddbc438612
iF <ifchen0@gmail.com>
2015-11-12 21:22:30 +0800

    ==RELEASE==

--- build/Release/plugins/portal-counts.meta.js
+++ build/Release/plugins/portal-counts.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @id             ingress-intel-total-conversion@jonatkins
-// @name           IITC: Ingress intel map total conversion
-// @version        0.25.2.20151112.131637
+// @id             iitc-plugin-portals-count@yenky
+// @name           IITC plugin: Show total counts of portals
+// @category       Info
+// @version        0.1.2.20151112.131637
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/total-conversion-build.meta.js
-// @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/total-conversion-build.user.js
-// @description    [Release-2015-11-12-131637] Total conversion for the ingress intel map.
+// @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/plugins/portal-counts.meta.js
+// @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/plugins/portal-counts.user.js
+// @description    [Release-2015-11-12-131637] 依照該區域的Portal等級和陣營顯示圖表.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
