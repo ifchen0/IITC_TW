1d25e4592286a44cdad8e141fb78f7a07167a430
iF <ifchen0@gmail.com>
2015-11-27 23:19:53 +0800

    ==RELEASE==

---
+++ build/Release/plugins/guess-player-levels.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-guess-player-levels@breunigs
+// @name           IITC plugin: guess player level
+// @category       Info
+// @version        0.5.7.20151127.151725
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/plugins/guess-player-levels.meta.js
+// @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/plugins/guess-player-levels.user.js
+// @description    [Release-2015-11-27-151725] 嘗試從目前畫面中的數據猜測玩家的等級.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
