71389bc4287eb631b142223aec970953cf475e76
iF <ifchen0@gmail.com>
2016-09-07 09:31:18 +0800

    Add areas-under-attack.user.js plug-in.

---
+++ build/Release/plugins/areas-under-attack.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @id             iitc-plugin-areas-under-attack@bryndavies
+// @name           IITC plugin: highlight areas recently under attack
+// @category       調整
+// @version        0.1.1.20160907.12856
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/plugins/areas-under-attack.meta.js
+// @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/plugins/areas-under-attack.user.js
+// @description    [Release-2016-09-07-012856] Highlight areas under attack in last 15 mins by flashing portals, links and fields red. Uses chat data to determine attacked portals.
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
