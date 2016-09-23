8bd21da736f1c91bb58806f8d8ec69fd44d29acc
iF <ifchen0@gmail.com>
2016-09-23 15:01:04 +0800

    Change default background color to black because IITCm portal detail use same background color.

--- build/Release/total-conversion-build.meta.js
+++ build/Release/total-conversion-build.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @id             ingress-intel-total-conversion@jonatkins
 // @name           IITC: Ingress intel map total conversion
-// @version        0.25.2.20160923.55547
+// @version        0.25.2.20160923.65725
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/total-conversion-build.meta.js
 // @downloadURL    https://raw.githubusercontent.com/ifchen0/IITC_TW/master/build/Release/total-conversion-build.user.js
-// @description    [Release-2016-09-23-055547] Total conversion for the ingress intel map.
+// @description    [Release-2016-09-23-065725] Total conversion for the ingress intel map.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
