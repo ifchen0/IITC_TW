fba1408f4c2b90c19603fd6ad0b1994b9d4bd0d5
iF <ifchen0@gmail.com>
2017-02-17 02:39:18 -0600

    Merged revision(s) 5958-5962 from ress-intel-total-conversion/trunk:

--- code/artifact.js
+++ code/artifact.js
@@ -1,4 +1,4 @@
-﻿// ARTIFACT ///////////////////////////////////////////////////////
+// ARTIFACT ///////////////////////////////////////////////////////
 
 // added as part of the ingress #13magnus in november 2013, artifacts
 // are additional game elements overlayed on the intel map
@@ -96,6 +96,11 @@ window.artifact.processResult = function (portals) {
   for (var guid in portals) {
     var ent = portals[guid];
     var data = decodeArray.portalSummary(ent);
+    
+    if (!data.artifactBrief) {
+      // 2/12/2017 - Shard removed from a portal leaves it in artifact results but has no artifactBrief
+      continue;
+    }
 
     // we no longer know the faction for the target portals, and we don't know which fragment numbers are at the portals
     // all we know, from the portal summary data, for each type of artifact, is that each artifact portal is
